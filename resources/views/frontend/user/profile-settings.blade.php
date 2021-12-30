a4b303c7f4c6814fd81328364797d6f72833d1e0
MalikAbubakar890 <abubakar19417malik@gmail.com>
2021-12-31 00:04:00 +0500

    security card put

--- resources/views/frontend/user/profile-settings.blade.php
+++ resources/views/frontend/user/profile-settings.blade.php
@@ -79,8 +79,8 @@
                                   <li class="nav-item">
                                     <a href="#" class="nav-link"><img src="{{ asset('/front/img/side-nav/question.png')}}"
                                             alt="">Qusetions</a>
-                                    <span class="icon"><i class="arrow_carrot-down"></i></span>
-                                    <ul class="list-unstyled dropdown_nav">
+                                    <span class="icon" ><i class="arrow_carrot-down" ></i></span>
+                                    <ul class="list-unstyled dropdown_nav" >
                                         <li><a href="#">New Questions</a></li>
                                         <li><a href="#">Trending Questions</a></li>
                                         <li><a href="#">Must read Questions</a></li>
@@ -158,92 +158,52 @@
                                     </div>
                                 </div>
                             </div>
-                          <!--   <div class="widget-body">
-                                <p class="user-text">I am a Graphic Designer based in New York, specializing in User
-                                    Interface Design and Development. I build clean, appealing, and functional
-                                    interfaces which comply with the latest web standards. But that’s just a part of it.
-                                    Design is my life. It’s my five-star spa. It’s my roller-coaster. It’s something I
-                                    do before going to bed, and something I can’t wait to do in the mornings. Without
-                                    it, my world would be black and white.”</p>
+                             <div class="user-details-widget security">
+                                    <div class="card card-ask box-shadow">
+                                    <div class="card-body p-4">
+                                        <form enctype="multipart/form-data" method="POST" action="{{ url('/updateuserprofile') }}">
+                                            {{ csrf_field() }}
+                                            <h5 class="mb-4 text-uppercase"><i class="mdi mdi-account-circle mr-1"></i> Security </h5>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label for="firstname">Old Password</label>
+                                                        <input type="text" class="form-control" placeholder="First Name" name="name">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                            <div class="col-md-6">
+                                                     <div class="form-group">
+                                                        <label for="lastname">New Password</label>
+                                                        <input  type="text"  class="form-control" placeholder="N.Password">
+                                                    </div>
+                                                </div> 
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="firstname">Re-type Password</label>
+                                                        <input type="text"  class="form-control" placeholder="R.Password" >
+                                                    </div>
+                                                </div>
+                                                
+                                            </div> 
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                            <div class="text-right">
+                                                <button id="submitbutton" type="submit" class="btn btn-primary btn-block">Save</button>
+                                                 </div>
+                                            </div>
+                                                   
+                                                </div>
+                                                 
+                                            </div>                                                              
+                                            
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
 
-                                <h4>Basic Information</h4>
-                                <div class="row mt-3">
-                                    <div class="col-md-4">
-                                        <h6><img src="{{ asset('/front/img/user_details/calendar.png')}}" alt=""> Age</h6>
-                                        <p>25 Years</p>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <h6><img src="{{ asset('/front/img/user_details/badge.png')}}" alt=""> Experience</h6>
-                                        <p>5 Years</p>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <h6><img src="{{ asset('/front/img/user_details/phone.png')}}" alt=""> Phone</h6>
-                                        <p> <a href="tel:12345679">+1 (202) 456 789</a> </p>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <h6><img src="{{ asset('/front/img/user_details/location.png')}}" alt=""> Location</h6>
-                                        <p>Boston, MA, United States</p>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <h6><img src="{{ asset('/front/img/user_details/envelope.png')}}" alt=""> Email</h6>
-                                        <p><a href="mailto:gustavo@ama.com">gustavo@ama.com</a></p>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <h6><img src="{{ asset('/front/img/user_details/link.png')}}" alt=""> Visit site</h6>
-                                        <p> <a href="https://www.ama.com">www.ama.com</a></p>
-                                    </div>
-
-                                </div>
-                            </div> -->
-                        </div>
-                        <div class="row mt-30 gy-4 gy-xl-0">
-                            <div class="col-xl-3 col-md-6">
-                                <div class="qna-statistics">
-                                    <div>
-                                        <img src="{{ asset('/front/img/user_details/help-button.png')}}" alt="">
-                                    </div>
-                                    <div>
-                                        <p>Questions</p>
-                                        <h5 class="counter">15</h5>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="qna-statistics">
-                                    <div>
-                                        <img src="{{ asset('/front/img/user_details/comment.png')}}" alt="">
-                                    </div>
-                                    <div>
-                                        <p>Answers</p>
-                                        <h5 class="counter">9</h5>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="qna-statistics">
-                                    <div>
-                                        <img src="{{ asset('/front/img/user_details/crown.png')}}" alt="">
-                                    </div>
-                                    <div>
-                                        <p>Best Answers</p>
-                                        <h5 class="counter">299</h5>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="qna-statistics">
-                                    <div>
-                                        <img src="{{ asset('/front/img/user_details/star.png')}}" alt="">
-                                    </div>
-                                    <div>
-                                        <p>Points</p>
-                                        <h5 class="counter">5</h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </section>
 @endsection
