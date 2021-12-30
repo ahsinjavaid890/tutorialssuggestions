7dd55680983c895aeaf5e7aeb4dbbf92df9cce0b
MalikAbubakar890 <abubakar19417malik@gmail.com>
2021-12-30 15:53:46 +0500

    Change the sections

--- resources/views/frontend/categories.blade.php
+++ resources/views/frontend/categories.blade.php
@@ -29,7 +29,76 @@
 </script>
 @endsection
 @section('content')
-<section class="profile-section single-community">
+
+ <section class="banner-area-9 pt-lg-100 pt-55 pb-100 "
+            style="background-image: url(img/breadcrumb/banner-bg.png);">
+            <div class="container">
+                <div class="row pt-90">
+                    <div class=" col-lg-9 doc_banner_content text-center mx-auto">
+                        <h2 class="text-white"><span class="banner-title-h1 mb-2">Categories</span>
+                        </h2>
+                        
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+<section class="forum-category-area pt-100 bg-disable pb-lg-115 pb-100">
+    <div class="container">
+        <h2 class="section-title-h2 text-center mb-60 text-center wow fadeinUp">Amas Categories</h2>
+        <div class="row gy-lg-0 gy-4 justify-content-center">
+            <div class="col-custom wow fadeInUp">
+                <a href="#">
+                    <div class="single-category-widget">
+                        <img src="{{ asset('/front/img/home_three/forum-catagory-01.svg')}}" alt="icon">
+                        <h5>Getting started</h5>
+                    </div>
+                </a>
+            </div>
+            <div class="col-custom wow fadeInUp" data-wow-delay="0.3s">
+                <a href="#">
+                    <div class="single-category-widget">
+                        <img src="{{ asset('/front/img/home_three/forum-catagory-02.svg')}}" alt="icon">
+                        <h5>Announcements</h5>
+                    </div>
+                </a>
+            </div>
+            <div class="col-custom wow fadeInUp" data-wow-delay="0.5s">
+                <a href="#">
+                    <div class="single-category-widget">
+                        <img src="{{ asset('/front/img/home_three/forum-catagory-03.svg')}}" alt="icon">
+                        <h5>Authors</h5>
+                    </div>
+                </a>
+            </div>
+            <div class="col-custom wow fadeInUp" data-wow-delay="0.7s">
+                <a href="#">
+                    <div class="single-category-widget">
+                        <img src="{{ asset('/front/img/home_three/forum-catagory-04.svg')}}" alt="icon">
+                        <h5>Customers</h5>
+                    </div>
+                </a>
+            </div>
+            <div class="col-custom wow fadeInUp" data-wow-delay="0.9s">
+                <a href="#">
+                    <div class="single-category-widget">
+                        <img src="{{ asset('/front/img/home_three/forum-catagory-05.svg')}}" alt="icon">
+                        <h5>Technology</h5>
+                    </div>
+                </a>
+            </div>
+
+
+        </div>
+
+    </div>
+</section>
+
+
+
+
+<!-- <section class="profile-section single-community">
         <div class="container">
             <div class="row mt-100">
                 <div class="col-md-12">
@@ -85,5 +154,5 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
 @endsection
--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -32,7 +32,7 @@
 
 
 <!-- ==========Contact-Section========== -->
-<section class="contact-section">
+<!-- <section class="contact-section">
     <div class="container">
         <div class="row">
           <div class="col-md-12">
@@ -45,7 +45,6 @@
           </div>
       </div>
     </div>
-    <!-- <img class="img-left" src="assets/images/contact/img-left.png" alt=""> -->
     <img class="img-right" src="{{asset('/front/assets/images/contact/img-right.png')}}" alt="">
     <div class="container">
         <div class="row justify-content-center">
@@ -86,7 +85,167 @@
             </div>
         </div>
     </div>
-</section>
+</section> -->
+        <div class="mobile_main_menu dark_menu sticky-nav menu_one">
+            <div class="container">
+                <div class="mobile_menu_left">
+                    <button type="button" class="navbar-toggler mobile_menu_btn">
+                        <span class="menu_toggle ">
+                            <span class="hamburger">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </span>
+                        </span>
+                    </button>
+                    <a class="navbar-brand header_logo" href="index.html">
+                        <img class="sticky_logo " src="img/logo.png" srcset="img/logo-2x.png 2x" alt="logo">
+                        <img class="main_logo white_logo" src="img/logo-w.png" srcset="img/logo-w2x.png 2x" alt="logo">
+                    </a>
+                </div>
+                <div class="mobile_menu_right">
+                    <div class="right-nav">
+                        <div class="px-2 js-darkmode-btn" title="Toggle dark mode">
+                            <label for="something2" class="tab-btn tab-btns">
+                                <ion-icon name="moon"></ion-icon>
+                            </label>
+                            <label for="something2" class="tab-btn">
+                                <ion-icon name="sunny"></ion-icon>
+                            </label>
+                            <label class=" ball" for="something2"></label>
+                            <input type="checkbox" name="something2" id="something2" class="dark_mode_switcher">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="side_menu">
+            <div class="mobile_menu_header">
+                <div class="close_nav">
+                    <i class="icon_close"></i>
+                </div>
+                <div class="mobile_logo">
+                    <a class="navbar-brand header_logo me-0" href="index.html">
+                        <img class="sticky_logo main_logo" src="img/logo.png" srcset="img/logo-2x.png 2x" alt="logo">
+                        <img class="white_logo" src="img/logo-w.png" srcset="img/logo-w2x.png 2x" alt="logo">
+                    </a>
+                </div>
+            </div>
+            <div class="mobile_nav_wrapper">
+                <nav class="mobile_nav_top">
+                    <ul class="navbar-nav menu ms-auto">
+                        <li class="nav-item dropdown submenu ">
+                            <a href="index.html" class="nav-link dropdown-toggle">Home</a>
+                            <i class="arrow_carrot-down_alt2 mobile_dropdown_icon"></i>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item "><a href="index.html" class="nav-link">Demo 01</a></li>
+                                <li class="nav-item"><a href="index-2.html" class="nav-link">Demo 02</a></li>
+                                <li class="nav-item "><a href="index-3.html" class="nav-link">Demo 03</a></li>
+                                <li class="nav-item "><a href="index-4.html" class="nav-link">Demo 04</a></li>
+                                <li class="nav-item "><a href="index-5.html" class="nav-link">Demo 05</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown submenu active">
+                            <a class="nav-link dropdown-toggle" href="#">
+                                Pages
+                            </a>
+                            <i class="arrow_carrot-down_alt2 mobile_dropdown_icon"></i>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a href="Layout_Header-Search-1.html" class="nav-link">Header
+                                        Layout 01</a></li>
+                                <li class="nav-item"><a href="Layout_Header-Search-2.html" class="nav-link">Header
+                                        Layout 02</a></li>
+                                <li class="nav-item active"><a href="contact.html" class="nav-link">Contact</a></li>
+                                <li class="nav-item"><a href="typography.html" class="nav-link">Typography</a></li>
+                                <li class="nav-item"><a href="404-error.html" class="nav-link">404 Error</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown submenu ">
+                            <a class="nav-link dropdown-toggle" href="forums.html">
+                                Forum
+                            </a>
+                            <i class="arrow_carrot-down_alt2 mobile_dropdown_icon"></i>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item "><a href="forums.html" class="nav-link">Forums Root</a></li>
+                                <li class="nav-item"><a href="forum-topics.html" class="nav-link">Forum Topics</a></li>
+                                <li class="nav-item"><a href="forum-topics-2.html" class="nav-link">Forum Topics Two</a>
+                                </li>
+                                <li class="nav-item"><a href="forum-single.html" class="nav-link">Topic Details</a></li>
+                                <li class="nav-item"><a href="forum-profile.html" class="nav-link">User Profile</a></li>
+                                <li class="nav-item "><a href="add-question.html" class="nav-link">Add Question</a></li>
+                                <li class="nav-item"><a href="user-list.html" class="nav-link">User List</a></li>
+                                <li class="nav-item"><a href="user-details.html" class="nav-link">User Details</a></li>
+                                <li class="nav-item"><a href="badges.html" class="nav-link">User Badges</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown submenu">
+                            <a class="nav-link dropdown-toggle" href="#">
+                                Blog
+                            </a>
+                            <i class="arrow_carrot-down_alt2 mobile_dropdown_icon"></i>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a href="blog-grid.html" class="nav-link">Blog Grid</a></li>
+                                <li class="nav-item"><a href="blog-grid-two.html" class="nav-link">Blog Grid Two</a>
+                                </li>
+                                <li class="nav-item"><a href="blog-list.html" class="nav-link">Blog List</a></li>
+                                <li class="nav-item"><a href="blog-single.html" class="nav-link">Blog Details</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <section class="banner-area-9 pt-lg-100 pt-55 pb-100 "
+            style="background-image: url(img/breadcrumb/banner-bg.png);">
+            <div class="container">
+                <div class="row pt-90">
+                    <div class=" col-lg-9 doc_banner_content text-center mx-auto">
+                        <h2 class="text-white"><span class="banner-title-h1 mb-2">Contact Us</span>
+                        </h2>
+                        
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="contact_area sec_pad">
+            <div class="container">
+                <div class="contact_info">
+                    <div class="section_title text-start">
+                        <h2 class="h_title wow fadeInUp">Contact US</h2>
+                        <p>Please email us, we’ll happy to assist you.</p>
+                    </div>
+                    <form action="#" class="contact_form">
+                        
+                        <div class="row contact_fill">
+                            <div class="col-lg-4 form-group">
+                                <h6>Full Name</h6>
+                                <input type="text" class="form-control" name="name" id="name"
+                                    placeholder="Enter your name here">
+                            </div>
+                            <div class="col-lg-4 form-group">
+                                <h6>Email</h6>
+                                <input type="email" class="form-control" name="email" id="email"
+                                    placeholder="info@Foram.com">
+                            </div>
+                            <div class="col-lg-4 form-group">
+                                <h6>Phone no</h6>
+                                <input type="tel" class="form-control" name="tel" id="phone" placeholder="+462">
+                            </div>
+                            <div class="col-lg-12 form-group">
+                                <h6>Message</h6>
+                                <textarea class="form-control message" id="message"
+                                    placeholder="Enter Your Text ..."></textarea>
+                            </div>
+                            <div class="col-lg-12 form-group">
+                                <button type="submit" class="btn action_btn thm_btn">Send Message</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
 <script type="text/javascript">
     function contactus()
     {
